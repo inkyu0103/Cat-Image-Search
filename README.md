eefc32a1787e0d47601e68defa14a45cdc0f2545
Inkyu Kim <inkyu0103@gmail.com>
2021-05-05 18:29:59 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Cat Image
+# Cat Image Search
+
+
+
+## 1. 컴포넌트 
+
+<br>
+<p align="center"><img src="./Component.png" width="500px" height="auto"></p>
+
+
+<br>
+
+- 컴포넌트를 작성하는데 가장 중요하게 생각한 부분 
+  
+  - 데이터는 최상단에서만 관리할 수 있어야 한다.
+  - 각 컴포넌트는 독립적인 구조를 가지고 있어야 한다.
+           
--- SearchResult.js
+++ SearchResult.js
@@ -2,14 +2,13 @@ export function SearchResult ({$target}) {
     this.initialState = []
     
     this.setState = (nextState) =>{
-        this.initialState = [...this.initialState,...nextState]
+        this.initialState = nextState
         this.render()
     }
 
     this.render = () =>{
         $target.innerHTML = this.initialState.map(ele =>{
             const {url,id} = ele
-            console.log(url)
             return `<div id="${id}"><img src="${url}"/></div>`
         }).join("")
     }
